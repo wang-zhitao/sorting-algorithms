c3a277bcd73738ad2350327b0f98a261b098037c
wang-zhitao <1614214972@qq.com>
2024-09-26 15:31:11 +0800

    modify

--- sort/07_heapSort.cpp
+++ sort/07_heapSort.cpp
@@ -1,17 +1,34 @@
 #include "head.h"
 
-void heapAdjust(vector<int> &vec, int len, int parent) {
-    int largest = parent;
-    int left = 2 * parent + 1;
-    int right = 2 * parent + 2;
+// void heapAdjust(vector<int> &vec, int len, int parent) {
+//     int largest = parent;
+//     int left = 2 * parent + 1;
+//     int right = 2 * parent + 2;
 
-    if (left < len && vec[left] > vec[largest])
-        largest = left;
-    if (right < len && vec[right] > vec[largest])
-        largest = right;
-    if (largest != parent) {
+//     if (left < len && vec[left] > vec[largest])
+//         largest = left;
+//     if (right < len && vec[right] > vec[largest])
+//         largest = right;
+//     if (largest != parent) {
+//         swap(vec[largest], vec[parent]);
+//         heapAdjust(vec, len, largest);
+//     }
+// }
+
+void heapAdjust(vector<int> &vec, int len, int parent) {
+
+    while (true) {
+        int largest = parent;
+        int left = 2 * parent + 1;
+        int right = 2 * parent + 2;
+        if (left < len && vec[left] > vec[largest])
+            largest = left;
+        if (right < len && vec[right] > vec[largest])
+            largest = right;
+        if (largest == parent)
+            break;
         swap(vec[largest], vec[parent]);
-        heapAdjust(vec, len, largest);
+        parent = largest;
     }
 }
 
